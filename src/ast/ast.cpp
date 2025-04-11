df12804c445a82d91316be72720495d55dd641d1
lecluyse2000 <lecluyse20@gmail.com>
2025-04-11 12:16:43 -0500

    Broke mathAST construction into helper functions

--- src/ast/ast.cpp
+++ src/ast/ast.cpp
@@ -40,8 +40,8 @@ void BoolAST::build_ast(const std::span<const Types::Token> prefix_expression) n
 
 [[nodiscard]] bool BoolAST::evaluate() const { return m_root->evaluate(); }
 
-std::unique_ptr<MathNodes::MathNode> build_value_node(const std::span<const Types::Token>& prefix_expression, const bool floating_point,
-                                                      std::size_t& index, const Token current_token) {
+std::unique_ptr<MathNodes::MathNode> MathAST::build_value_node(const std::span<const Types::Token>& prefix_expression, const bool floating_point,
+                                                               std::size_t& index, const Token current_token) {
         if (is_math_var(current_token)) {
             return std::make_unique<MathNodes::ValueMNode>(current_token); 
         }
